ae1a60b9d40ee86b611e791880fd6348afeee865
Mark Santiago <83982029+Oxbower@users.noreply.github.com>
2024-02-16 19:35:53 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+# file for input, handle processing in another python file
+import matplotlib
+import numpy as np
+
+from preprocess_input import join, MnistDataloader
+from neural_net import NN
+
+'''
+    Attributions:
+    MNIST data loader from https://www.kaggle.com/code/hojjatk/read-mnist-dataset/notebook
+'''
+
+
+#
+# Verify Reading Dataset via MnistDataloader class
+#
+import random
+import matplotlib.pyplot as plt
+
+#
+# Set file paths based on added MNIST Datasets
+#
+input_path = '/Users/marksantiago/Downloads/archive'
+training_images_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
+training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
+test_images_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
+test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+
+
+#
+# Helper function to show a list of images with their relating titles
+#
+def show_images(images, title_texts):
+    cols = 4
+    rows = int(len(images) / cols) + 1
+    plt.figure(figsize=(60, 60))
+    index = 1
+    for x in zip(images, title_texts):
+        image = x[0]
+        title_text = x[1]
+        plt.subplot(rows, cols, index)
+        plt.imshow(image)
+        if title_text != '':
+            plt.title(title_text, fontsize=10)
+        index += 1
+        plt.xticks([0, 27])
+        plt.yticks([0, 27])
+    plt.tight_layout(h_pad=40, w_pad=5)
+    plt.ioff()
+    plt.show()
+
+#
+# Show some random training and test images
+#
+def display_input():
+    images_2_show = []
+    titles_2_show = []
+    for i in range(0, 10):
+        r = random.randint(1, 60000)
+        images_2_show.append(x_train[r])
+        titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))
+
+    for i in range(0, 5):
+        r = random.randint(1, 10000)
+        images_2_show.append(x_test[r])
+        titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))
+
+    show_images(images_2_show, titles_2_show)
+
+
+#
+# Load MINST dataset
+#
+mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
+                                   test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+def main():
+    # display_input()
+    NN(x_train, y_train, 5000, 0.00001, 3, 10)
+
+
+if __name__ == '__main__':
+    main()
---
+++ neural_net.py
@@ -0,0 +1,33 @@
+# predictive anal using neural nets :)
+
+import numpy as np
+
+
+def sigmoid_f():
+    return 1 / (1 + np.exp())
+
+
+def sigmoid_d():
+    pass
+
+
+def tanh_f():
+    return np.tanh()
+
+
+def cost():
+    pass
+
+def loss():
+    pass
+
+
+def gradient():
+    pass
+
+
+# neural net call func
+def NN(X, t, epoch, lr, n_hidden, n_output):
+    for _ in epoch:
+        pass
+    return cost_list, y, theta
