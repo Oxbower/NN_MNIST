35eb0469e7f5085882ef92e61a6d7401ad776849
Oxbower <83982029+Oxbower@users.noreply.github.com>
2024-02-16 20:01:04 -0400

    Path rework
    Moved global vars to local scope

--- main.py
+++ main.py
@@ -18,22 +18,12 @@ import random
 import matplotlib.pyplot as plt
 
 #
-# Set file paths based on added MNIST Datasets
-#
-input_path = '/Users/marksantiago/Downloads/archive'
-training_images_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
-training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
-test_images_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
-test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
-
-
-#
 # Helper function to show a list of images with their relating titles
 #
 def show_images(images, title_texts):
     cols = 4
     rows = int(len(images) / cols) + 1
-    plt.figure(figsize=(60, 60))
+    plt.figure(figsize=(30, 30))
     index = 1
     for x in zip(images, title_texts):
         image = x[0]
@@ -41,18 +31,15 @@ def show_images(images, title_texts):
         plt.subplot(rows, cols, index)
         plt.imshow(image)
         if title_text != '':
-            plt.title(title_text, fontsize=10)
+            plt.title(title_text, fontsize=30)
         index += 1
-        plt.xticks([0, 27])
-        plt.yticks([0, 27])
-    plt.tight_layout(h_pad=40, w_pad=5)
     plt.ioff()
     plt.show()
 
 #
 # Show some random training and test images
 #
-def display_input():
+def display_input(x_train, y_train, x_test, y_test):
     images_2_show = []
     titles_2_show = []
     for i in range(0, 10):
@@ -67,17 +54,20 @@ def display_input():
 
     show_images(images_2_show, titles_2_show)
 
-
-#
-# Load MINST dataset
-#
-mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
-                                   test_labels_filepath)
-(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
-
 def main():
-    # display_input()
-    NN(x_train, y_train, 5000, 0.00001, 3, 10)
+    input_path = input("Path to MNIST archive folder:\t")
+    training_images_filepath = join(input_path, 'train-images-idx3-ubyte\\train-images-idx3-ubyte')
+    training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte\\train-labels-idx1-ubyte')
+    test_images_filepath = join(input_path, 't10k-images-idx3-ubyte\\t10k-images-idx3-ubyte')
+    test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte\\t10k-labels-idx1-ubyte')
+
+    mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
+                                       test_labels_filepath)
+    (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+    display_input(x_train, y_train, x_test, y_test)
+
+    #NN(x_train, y_train, 5000, 0.00001, 3, 10)
 
 
 if __name__ == '__main__':
